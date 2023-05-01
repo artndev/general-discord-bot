598049614df3d4d6a0e466f4f5ea625883f8f732
artndev <97910780+artndev@users.noreply.github.com>
2023-05-01 13:07:18 +0300

    test.js rework

--- src/db/test.js
+++ src/db/test.js
@@ -1,26 +1,7 @@
-const { insert, findBy, differenceBetween, updateQuote, isDaily } = require('./setup.js')
+// const { isDaily } = require('./setup.js')
 
 
-// insert("discord#6666")
+// isDaily("discord#1111")
 //     .then((data) => console.log(data))
 
-// differenceBetween("discord#1111", "discord#0000").then((data) => console.log(data))
-
-
-isDaily("discord#1111")
-    .then((data) => console.log(data))
-
-    // const isDailyFunc = full_name => {
-    //     return findByFunc(full_name)
-    //         .then(async (data) => { 
-    //             const res = dateToHours(new Date()) - dateToHours(data[0].date)
-    //             if (res >= 24) {
-    //                 await updateFunc(full_name)
     
-    //                 return true
-    //             }
-    //             else
-    //                 return false
-    //         })
-    //         .catch(err => { throw err })
-    // }
